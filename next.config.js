25864e65c6d17d5eddc73ce44ed9c5f787888269
GeSamaras <georgiosamaras@hotmail.com>
2024-04-30 14:25:30 +0200

    shadui and sentry set up

--- next.config.js
+++ next.config.js
@@ -40,8 +40,8 @@ const config = withSentryConfig(
 
     // Suppresses source map uploading logs during build
     silent: true,
-    org: "t3gg",
-    project: "t3-gallery-video",
+    org: "mimi",
+    project: "mimireact-proj",
   },
   {
     // For all available options, see:
