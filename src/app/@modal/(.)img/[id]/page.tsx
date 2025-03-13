8c59ed2171c892e8fa7a1d7687f8059c1381633b
GeSamaras <georgiosamaras@hotmail.com>
2025-03-13 13:45:49 +0100

    updated to next15, sync dynamic api needed

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,11 +1,17 @@
 import { Modal } from "./modal";
 import { FullPageImageView } from "~/common/full-page-image-view";
 
-export default async function PhotoModal({
-  params: { id: photoId },
-}: {
-  params: { id: string };
-}) {
+export default async function PhotoModal(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
+
+  const {
+    id: photoId
+  } = params;
+
   return (
     <Modal>
       <FullPageImageView photoId={photoId} />
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { FullPageImageView } from "~/common/full-page-image-view";
 
-export default function PhotoModal({
-  params: { id: photoId },
-}: {
-  params: { id: string };
-}) {
+export default async function PhotoModal(
+  props: {
+    params: Promise<{ id: string }>;
+  }
+) {
+  const params = await props.params;
+
+  const {
+    id: photoId
+  } = params;
+
   return (
     <div className="flex-col border-x-pink-600 p-12">
         <FullPageImageView photoId={photoId} />
